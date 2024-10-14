e3f6a5d677f0fc6a98b4045ffbc5a568e36ccd3a
cycheung73 <cycheung73@yahoo.com>
2024-10-14 12:39:50 -0400

    attributes-v3 - iteration on attributes-v2.  move point buy data into an h2 file database.  need to add HTML pages next to show and choose attributes.

---
+++ ttrpg/character/attributes-v3/src/main/java/org/csgeeks/attributes/AttributesApplication.java
@@ -0,0 +1,13 @@
+package org.csgeeks.attributes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttributesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttributesApplication.class, args);
+	}
+
+}
