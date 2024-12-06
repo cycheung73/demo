c70eed85774faee81eca3880cfde45c31526a09e
cycheung73 <cycheung73@yahoo.com>
2024-12-05 21:58:48 -0500

    Advent of Code Day 5

--- advent/2024/java/advent-code-2024-08/src/main/java/org/csgeeks/App.java
+++ advent/2024/java/advent-code-2024-08/src/main/java/org/csgeeks/App.java
@@ -4,8 +4,6 @@ import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 import java.util.ArrayList;
 import java.util.List;
 
---
+++ advent/2024/java/advent-code-2024-09/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#java -cp target/advent-code-2024-09-1.0-SNAPSHOT.jar org.csgeeks.App test.txt
+java -cp target/advent-code-2024-09-1.0-SNAPSHOT.jar org.csgeeks.App input.txt
